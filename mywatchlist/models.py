0af4fc21fc5c7c48c778ec832fc6a0a036eea50a
Muhammad Iqbal Dwitama <muhammad.iqbal111@ui.ac.id>
2022-09-19 23:59:37 +0700

    edit decimalfield

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -6,6 +6,6 @@ from django.db import models
 class MyWatchlist(models.Model):
     watched = models.BooleanField()
     title = models.CharField(max_length=50)
-    rating = models.DecimalField(max_digits=5, decimal_places=1)
+    rating = models.DecimalField(blank=True, null=True, max_digits=5, decimal_places=1)
     release_date = models.DateField()
     review = models.TextField()
